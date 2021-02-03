397881bb93367732b65234b5dcf1e372c1f84423
unknown <douglasfarmaangra@gmail.com>
2021-02-02 22:52:17 -0300

    create javascript

--- MaratonaDiscover/script.js
+++ MaratonaDiscover/script.js
@@ -50,22 +50,22 @@ const Transaction = {
 };
 
 const DOM = {
-  transactionsContainer: document.querySelector('#data-table tbody'),
+  transactionsContainer: document.querySelector("#data-table tbody"),
 
   addTransaction(transaction, index) {
     const tr = document.createElement("tr");
     tr.innerHTML = DOM.innerHTMLTransaction(transaction);
 
-    DOM.transactionsContainer.appendChild(tr)
+    DOM.transactionsContainer.appendChild(tr);
   },
   innerHTMLTransaction(transaction) {
-    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
-    const amount = Utils.formatCurrency(transaction.amount)
+    const amount = Utils.formatCurrency(transaction.amount);
 
     const html = `
       <td class="description">${transaction.description}</td>
-      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
         <img src="./assets/minus.svg" alt="Remover Transação">
@@ -73,14 +73,31 @@ const DOM = {
     `;
     return html;
   },
+
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = "Soma das Entradas";
+    document.getElementById("expenseDisplay").innerHTML = "Soma das saidas";
+    document.getElementById("totalDisplay").innerHTML = "Soma das saidas";
+  },
 };
 
 const Utils = {
   formatCurrency(value) {
-    const signal = Number(value) < 0 ? "-" : ""
-  }
-}
+    const signal = Number(value) < 0 ? "-" : "";
 
-transactions.forEach(function(transaction) {
-  DOM.addTransaction(transaction)
-})
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+};
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction);
+});
